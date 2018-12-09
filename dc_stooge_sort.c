16aa3b79cc2bc4f156e20f45ed46768c6679501f
anichucai <43823759+anichucai@users.noreply.github.com>
2018-12-09 13:28:40 -0300

    Create dc_stooge_sort.c

---
+++ dc_stooge_sort.c
@@ -0,0 +1,17 @@
+//EJERCIOCIO DE DIVISION Y CONQUIESTA
+
+/*CONSIGNA: Del primer recuperatorio del primer parcialito, segundo cuatrimestre de 2013
+El StoogeSort es un algoritmo de ordenamiento basado en Los Tres Chiflados que funciona
+de la siguiente forma:
+Si el primer elemento es m´as grande que el ultimo, los intercambia.
+Si la cantidad de elementos del vector es mayor o igual a 3:
+1. Llama a StoogeSort recursivamente con los primero dos tercios del vector (desde 0
+hasta 2/3 ∗ cantidad)
+2. Llama a StoogeSort recursivamente con los segundos dos tercios (desde 1/3∗cantidad,
+hasta cantidad)
+3. Vuelve a llamar recursivamente a StoogeSort con los primeros dos tercios del vector.
+Utilizando el teorema maestro, calcular el orden del algoritmo, suponiendo que la operaci´on de
+partir es de tiempo constante.
+*/
+
+// a=3 b=3 c=0 -> log3(3)>0 ->
