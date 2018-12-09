a4fa709794f71e48219b3dda50667de868cdb2e9
anichucai <43823759+anichucai@users.noreply.github.com>
2018-12-09 12:36:37 -0300

    Update pila_partir.c

--- pila_partir.c
+++ pila_partir.c
@@ -1,6 +1,11 @@
 #EJERCICIO PILA
 
-/*CONSIGNA: */
+/*CONSIGNA: Del primer parcialito del segundo cuatrimestre de 2012
+Escribir una funci´on que reciba una pila y un par´ametro n con la cantidad de elementos
+en la pila y la “parta” en dos: La que es recibida por par´ametro contendr´a la primera mitad
+(elementos de 0 a n/2-1), y la pila devuelta por la funci´on tendr´a los elementos de n/2 a n-1,
+manteniendo el orden de la pila original. (Se pueden usar las estructuras auxiliares que crean
+convenientes)*/
 
 pila_t* pila_partir(pila_t* pila_original, int n){
 
