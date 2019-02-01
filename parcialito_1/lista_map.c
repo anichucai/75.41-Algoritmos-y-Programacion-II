5352342654deec5b1e8bb493ee22d9ed5b4e8872
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-01 09:52:18 -0300

    Create lista_map.c

---
+++ parcialito_1/lista_map.c
@@ -0,0 +1,11 @@
+/*CONSIGNA: Si se tiene una implementacion de Lista Enlazada co las siguientes estructurad:
+
+typedef struct nodo_lista{
+  struct nodo_lista prox;
+  void* dato;
+} nodo_lista_t;
+
+typedef struct lista{
+  nodo_lista_t* prim;
+} lista_t;
+ 
