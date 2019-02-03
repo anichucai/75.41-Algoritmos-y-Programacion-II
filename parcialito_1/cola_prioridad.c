76c68854481f2b8f092fa661267bed561c5c6db8
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-02 22:37:58 -0300

    Update cola_prioridad.c

--- parcialito_1/cola_prioridad.c
+++ parcialito_1/cola_prioridad.c
@@ -12,4 +12,51 @@ es decir, no deben salir elementos comunes de la estructura si no salieron previ
 elementos con prioridad.
 Todas las primitivas deben funcionar en tiempo contasnte.*/
 
+typedef struct cola_p{
+  cola_t* comun;
+  cola_t* prioridad;
+} cola_p_t*;
+
 cola_p_t* cola_p_crear(){
+  
+  cola_p_t* cola = malloc(sizeof(cola_t));
+  if (!cola) return NULL;
+  
+  cola->comun = cola_crear();
+  if (!cola->comun){
+    free(cola);
+    return NULL;
+  }
+  
+  cola->prioridad = cola_crear();
+  if (!cola->prioridad){
+    cola_destrir(cola->comun);
+    free(cola);
+    return NULL;
+  }
+  
+  return cola;
+
+}
+
+bool cola_p_encolar_comun(cola_p_t* cola, void* dato){
+  return cola_encolar(cola->comun, dato);
+}
+
+bool cola_p_encolar_prioridad(cola_p_t* cola, void* dato){
+  return cola_prioridad(cola->prioridad, dato);
+}
+
+void* desencolar(cola_p_t* cola){
+  return (!cola_esta_vacia(cola->prioridad)) ? cola_desencolar(cola->prioridad) : cola_desencolar(cola->comun);
+}
+
+bool cola_p_esta_vacia(cola_p_t* cola){
+  return cola_esta_vacia(cola->prioridad) && cola_esta_vacia(cola->prioridad);
+}
+
+void cola_p_destruir(cola_p_t* cola){
+  cola_destruir(cola->prioridad);
+  cola_destriuir(cola->comun);
+  free(cola);
+}
