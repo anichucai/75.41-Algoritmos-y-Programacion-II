5bc40c0161a071b5a755fea06830b23fb0698714
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-02 22:13:09 -0300

    Update lista_map.c

--- parcialito_1/lista_map.c
+++ parcialito_1/lista_map.c
@@ -1,4 +1,4 @@
-/*CONSIGNA: Si se tiene una implementacion de Lista Enlazada co las siguientes estructurad:
+/*CONSIGNA: Si se tiene una implementacion de Lista Enlazada con las siguientes estructura:
 
 typedef struct nodo_lista{
   struct nodo_lista prox;
@@ -8,4 +8,19 @@ typedef struct nodo_lista{
 typedef struct lista{
   nodo_lista_t* prim;
 } lista_t;
- 
+
+a) Implementar un nueva primitiva "lista_map" que reciba como parametro un puntero a una
+funcion f (que tome y devuelva un numero entero), y como segundo parametro un puntero a 
+lista_t. lista_map debe reemplazar cada uno de los valores  de la lista por el resultado
+de aplicar la funcion f a dichos valores.
+b) Muestre un ejemplo de invocacion a la primitiva lista_map con una funcion que eleve al
+cuadrado.
+*/
+
+void lista_map( (int) f (int) , lista_t* lista){
+  nodo_t* act = lista->prim;
+  while(act){
+    act->dato = f (act->dato);
+    act = act->prox;
+  }
+}
