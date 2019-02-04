3ffb0aa94d4f74a9b5433e2b8427de37d49c72d7
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-03 22:53:00 -0300

    Update grep.c

--- TPs/TP1/grep.c
+++ TPs/TP1/grep.c
@@ -60,32 +60,3 @@ void grep(char* clave, int n, FILE* archivo){
 
 	cola_destruir(cola,free);
 }
- 
- 
-
-int ejecutar_grep(int argc,char* argv[]){
-
-	if (argc<3 || argc>4){
-		fprintf(stderr, "%s", "Cantidad de parametros erronea");
-		return 1;
-	}
-	
-	if (strcmp(argv[2],"0")!=0 && atoi(argv[2])==0) {
-		fprintf(stderr, "%s", "Tipo de parametro incorrecto");
-		return 1;
-	}
-
-	if (argc==4){
-		FILE* archivo = fopen(argv[3], "r");
-		if (!archivo){
-			fprintf(stderr, "%s", "No se puede ver el archivo indicado");
-			return 1;
-		}
-		grep(argv[1], atoi(argv[2]), archivo);
-		fclose(archivo);
-	} else {
-		grep(argv[1], atoi(argv[2]), stdin);
-	}
-
-	return 0;
-}
