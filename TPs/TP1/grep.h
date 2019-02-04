08ad304964cea521fc9d47525c1ff21e74ecfff4
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-03 22:53:52 -0300

    Update grep.h

--- TPs/TP1/grep.h
+++ TPs/TP1/grep.h
@@ -4,6 +4,31 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int ejecutar_grep(int argc, char* argv[]);
+int ejecutar_grep(int argc,char* argv[]){
+
+	if (argc<3 || argc>4){
+		fprintf(stderr, "%s", "Cantidad de parametros erronea");
+		return 1;
+	}
+	
+	if (strcmp(argv[2],"0")!=0 && atoi(argv[2])==0) {
+		fprintf(stderr, "%s", "Tipo de parametro incorrecto");
+		return 1;
+	}
+
+	if (argc==4){
+		FILE* archivo = fopen(argv[3], "r");
+		if (!archivo){
+			fprintf(stderr, "%s", "No se puede ver el archivo indicado");
+			return 1;
+		}
+		grep(argv[1], atoi(argv[2]), archivo);
+		fclose(archivo);
+	} else {
+		grep(argv[1], atoi(argv[2]), stdin);
+	}
+
+	return 0;
+}
 
 #endif
