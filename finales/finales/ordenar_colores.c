c965c5527c011b10d909235f6ceea998399958cb
anichucai <43823759+anichucai@users.noreply.github.com>
2019-02-12 16:03:17 -0300

    consigna

---
+++ finales/finales/ordenar_colores.c
@@ -0,0 +1,6 @@
+/*CONSIGNA: Se tiene una secuencia de n pares de la forma (número,color),
+ordenada por número, y donde color pertenece al conjunto {Rojo, Verde, Azul}.
+Escribir un algoritmo O(n) que deje todos los rojos, luego todos los verdes,
+y luego todos los azules, pero donde los números dentro de cada color siga
+ordenado: (por ejemplo (A,1),(R,2),(R,3),(A,4)→(R,2),(R,3),(A,1),(A,4)).
+Codificarlo en C.*/ 
